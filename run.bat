88704ed1f27c8d4315cf633c9f424cb5e6c6cc05
AnushaNandi <anushavani@yahoo.com>
2020-05-14 16:38:11 -0500

    This is my first commit for my first project

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd D:\Maddy_Eclipse\Practice_Eclipse\Maven_Project
+mvn clean install
---
+++ src/test/java/demo/Demo2Test.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo2Test {
+	WebDriver d;
+	@Test
+	public void titletest()
+	{
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/Drivers/chromedriver.exe");
+		d=new ChromeDriver();
+		d.get("http://www.gmail.com");
+		System.out.println("The title of the page is : "+d.getTitle());
+		d.quit();
+	}
+}
